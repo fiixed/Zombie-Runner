e8bee12fc83651fb2e6832785d4c89580e0f4483
Andrew Bell <fiixed@gmail.com>
2016-09-19 16:19:32 -0500

    Player scaling and respawn points

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Transform playerSpawnPoints;
+	public bool reSpawn = false;
+
+	private bool lastToggle = false;
+	private Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+		
+	}
+
+	void Update() {
+		if (lastToggle != reSpawn) {
+			ReSpawn();
+			reSpawn = false;
+		} else {
+			lastToggle = reSpawn;
+		}
+	}
+	
+	private void ReSpawn() {
+		Transform sp = spawnPoints[Random.Range(1, spawnPoints.Length)];
+		transform.position = sp.transform.position;
+
+	}
+}
