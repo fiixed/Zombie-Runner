1fdb7255efd5ba919304dc7e5bfe05ee287745d7
Andrew Bell <fiixed@gmail.com>
2016-09-19 18:45:18 -0500

    added helicopter and 3D sounds

---
+++ Assets/Helicopter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip callSound;
+
+	private bool called = false;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Call Heli")) {
+			called = true;
+			audioSource.clip = callSound;
+			audioSource.Play();
+		} 
+	}
+}
---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera eyes;
+	private float defaultFOV;
+	
+	// Use this for initialization
+	void Start () {
+		eyes = GetComponent<Camera>();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if (Input.GetKeyDown(KeyCode.V)) {
+		// 	eyes.fieldOfView = defaultFOV / (float)1.5;
+		// } 
+		// if (Input.GetKeyUp(KeyCode.V)) {
+		// 	eyes.fieldOfView = defaultFOV;
+		// }
+
+		if (Input.GetButton("Zoom")) {
+			eyes.fieldOfView = defaultFOV / (float)1.5;
+		} else {
+			eyes.fieldOfView = defaultFOV;
+		}
+	}
+}
